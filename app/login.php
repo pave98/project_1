7aa6d85fa8e1837ed653fc9c491c6bbc6b0ce122
pave <paavo.porkka@cs.tamk.fi>
2018-10-25 12:45:26 +0300

    More stuff

---
+++ app/login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+    <div class="header">
+        <h2>Login</h2>
+    </div>
+
+    <form method="post" action="login.php">
+        <!-- Display validation errors here -->
+        <?php include('errors.php'); ?>
+        <div class="input-group">
+            <label>Username</label>
+            <input type="text" name="username">
+        </div>
+        <div class="input-group">
+            <label>Password</label>
+            <input type="password" name="password">
+        </div>
+        <div class="input-group">
+            <button type="submit" name="login" class="btn">Login</button>
+        </div>
+        <p>
+            Not yet a member? <a href="register.php">Sign up</a>
+        </p>
+    </form>
+</body>
+</html>
