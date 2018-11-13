34691390c521d60b3ada17f3014bb03ed7a7e68a
pave <paavo.porkka@cs.tamk.fi>
2018-11-13 15:39:14 +0200

    kaikki tavara

---
+++ app/footer.php
@@ -0,0 +1,6 @@
+    <footer>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum molestie.</p>
+    </footer>
+</div>
+</body>
+</html>
---
+++ app/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Projekti1</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <div class="container">
+        <header>
+            <h1>RKC</h1>
+        </header>
+        <nav>
+            <ul>
+                <li>
+                    <a href="login.php">Login</a>
+                </li>
+                <li>
+                    <a href="javascript:void(0)">Joukkue</a>
+                </li>
+                <li>
+                    <a href="javascript:void(0)">Yhteystiedot</a>
+                </li>
+            </ul>
+        </nav>
