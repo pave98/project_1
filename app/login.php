a843ce7f59e5ef843314a31ad32adc610cafea77
pave <paavo.porkka@cs.tamk.fi>
2018-11-13 22:10:06 +0200

    lissee

--- app/login.php
+++ app/login.php
@@ -1,13 +1,13 @@
 <?php
-    require "header.php";
+    require "../app/header.php";
 ?>
     <div class="header">
         <h2>Login</h2>
     </div>
 
-    <form method="post" action="login.php">
+    <form method="post" action="../app/login.php">
         <!-- Display validation errors here -->
-        <?php include('errors.php'); ?>
+        <?php include('../app/errors.php'); ?>
         <div class="input-group">
             <label>Username</label>
             <input type="text" name="username">
@@ -19,11 +19,8 @@
         <div class="input-group">
             <button type="submit" name="login" class="btn">Login</button>
         </div>
-        <p>
-            Not yet a member? <a href="register.php">Sign up</a>
-        </p>
     </form>
 
 <?php
-    require "footer.php";
+    require "../app/footer.php";
 ?>
