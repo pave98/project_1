65d989981f5e0ecbca826e14b441df48ec8ef238
nikoh <niko.hienonen@gmail.com>
2018-11-14 12:04:28 +0200

    Update app/team/index.php

---
+++ app/team/index.php
@@ -0,0 +1,11 @@
+<?php
+    require "header.php";
+?>
+    <section class="s1">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam lacinia consectetur est. Ut vitae risus at nisi iaculis condimentum. Vivamus consectetur augue quam, ut vulputate enim commodo sit amet. Etiam tempor metus et blandit mollis. Ut id aliquam massa. Curabitur justo arcu, cursus quis ipsum sed, dictum bibendum odio. Ut blandit, ipsum hendrerit gravida gravida, nulla mauris consequat libero, non sagittis turpis neque sed dolor. Cras magna velit, commodo sit amet vulputate sed, pulvinar sit amet turpis.</p>
+    </section>
+    
+
+<?php
+    require "footer.php";
+?>
