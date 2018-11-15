902a13d7f24df49bcc6e0a52af2725e7d7f7aa78
Niko Hienonen <niko.hienonen@gmail.com>
2018-11-15 10:58:45 +0200

    vitun nastaa

--- app/index.php
+++ app/index.php
@@ -2,7 +2,11 @@
     require "header.php";
 ?>
     <section class="s1">
+<<<<<<< refs/remotes/origin/master
         <h1 class="motto">Yhdessä Runkulla</h1>
+=======
+        <h1>Yhdessä Runkulla</h1>
+>>>>>>> vitun nastaa
 
     </section>
     
