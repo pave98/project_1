74369c94e3fe8b370a279a641e9a6d9a3ec226d6
Paavo Porkka <paavo.porkka@cs.tamk.fi>
2018-11-27 19:28:37 +0200

    Added password reset to users

--- admin/create_user.php
+++ admin/create_user.php
@@ -51,7 +51,7 @@
                 <input type="textarea" name="description" value="<?php echo $description; ?>">
             </div>
             <div class="input-group">
-                <button type="submit" class="btn" name="register_btn"> + Create user</button>
+                <button type="submit" class="btn" name="register_btn"> Create user</button>
             </div>
         </form>
     </section>
