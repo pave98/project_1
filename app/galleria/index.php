0218287931ff101ce3289ab9888784530363ce1a
Niko Hienonen <niko.hienonen@gmail.com>
2018-12-04 17:10:16 +0200

    keinosen kikkailut

--- app/galleria/index.php
+++ app/galleria/index.php
@@ -2,19 +2,20 @@
     require "../header.php";
 ?>
     <section class="s1">
+    <h1>Galleria</h1>
         <div class="gallery">
             <div class="main-img">
-                <img src='http://localhost/project_1/app/images/uploads/img8.jpg' id="current">
+                <img src='http://localhost/project_1/app/images/uploads/img8.jpeg' id="current">
             </div>
             <div class="images">
-                <img src='http://localhost/project_1/app/images/uploads/img8.jpg'>
+                <img src='http://localhost/project_1/app/images/uploads/img8.jpeg'>
                 <img src='http://localhost/project_1/app/images/uploads/img7.jpg'>
                 <img src='http://localhost/project_1/app/images/uploads/img6.jpeg'>
                 <img src='http://localhost/project_1/app/images/uploads/img5.jpeg'>
                 <img src='http://localhost/project_1/app/images/uploads/img4.jpeg'>
                 <img src='http://localhost/project_1/app/images/uploads/img3.jpeg'>
                 <img src='http://localhost/project_1/app/images/uploads/img2.jpeg'>
-                <img src='http://localhost/project_1/app/images/uploads/img1.jpeg'>
+                <img src='http://localhost/project_1/app/images/uploads/img1.jpg'>
             </div>
         </div>
         <script src="http://localhost/project_1/app/galleria/galleryjs.js"></script>
