d3dba0dbfc1cc2ff6e2db6c53ab1139265799b73
Paavo Porkka <paavo.porkka@cs.tamk.fi>
2018-12-05 08:33:22 +0200

    Nimenhuuto valmiina

--- doc/rkc.sql
+++ doc/rkc.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Dec 01, 2018 at 03:52 PM
+-- Generation Time: Dec 05, 2018 at 06:32 AM
 -- Server version: 5.7.21
 -- PHP Version: 5.6.35
 
@@ -25,26 +25,46 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `attending`
+--
+
+DROP TABLE IF EXISTS `attending`;
+CREATE TABLE IF NOT EXISTS `attending` (
+  `event_id` int(255) NOT NULL,
+  `user_id` int(255) NOT NULL,
+  `decision` varchar(255) NOT NULL,
+  PRIMARY KEY (`event_id`,`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `attending`
+--
+
+INSERT INTO `attending` (`event_id`, `user_id`, `decision`) VALUES
+(3, 7, 'imIn'),
+(3, 6, 'imOut');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `events`
 --
 
 DROP TABLE IF EXISTS `events`;
 CREATE TABLE IF NOT EXISTS `events` (
-  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `event_id` int(255) NOT NULL AUTO_INCREMENT,
   `eventType` varchar(255) NOT NULL,
   `description` varchar(255) NOT NULL,
   `location` varchar(255) NOT NULL,
   `time` datetime(6) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`event_id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `events`
 --
 
-INSERT INTO `events` (`id`, `eventType`, `description`, `location`, `time`) VALUES
-(1, 'treeni', 'Perkeleessti', 'HelvetissÃ¤', '2018-11-30 13:00:00.000000'),
-(2, 'peli', 'minÃ¤ olen testi', 'HelvetissÃ¤', '2018-11-15 15:15:00.000000'),
+INSERT INTO `events` (`event_id`, `eventType`, `description`, `location`, `time`) VALUES
 (3, 'treeni', 'Niko on GAY', 'Perse', '2018-11-23 11:44:00.000000');
 
 -- --------------------------------------------------------
@@ -55,7 +75,7 @@ INSERT INTO `events` (`id`, `eventType`, `description`, `location`, `time`) VALU
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `user_id` int(255) NOT NULL AUTO_INCREMENT,
   `username` varchar(255) NOT NULL,
   `user_type` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
@@ -63,14 +83,14 @@ CREATE TABLE IF NOT EXISTS `users` (
   `firstName` varchar(255) NOT NULL,
   `lastName` varchar(50) NOT NULL,
   `description` text NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`user_id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `user_type`, `password`, `email`, `firstName`, `lastName`, `description`) VALUES
+INSERT INTO `users` (`user_id`, `username`, `user_type`, `password`, `email`, `firstName`, `lastName`, `description`) VALUES
 (7, 'testi', 'user', '9627df7a4a5b849f67fce863e82adc71', 'testi@testi.testi', 'testi', 'testi', 'minÃ¤ olen testi'),
 (6, 'admin', 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin@admin.com', 'admin', 'admin', 'admin'),
 (8, 'SAATANA', 'user', '99995eee3f56185f956b12b5973d6386', 'tuomensaloessi@gmail.com', 'Essi', 'Tuomensalo', 'ESSI ON SAATANA'),
