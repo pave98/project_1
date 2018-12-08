a8bdff95b1f7c1b01e320b534d2f13579dd109de
Paavo Porkka <paavo.porkka@cs.tamk.fi>
2018-12-08 16:20:36 +0200

    25

--- app/nimenhuuto/index.php
+++ app/nimenhuuto/index.php
@@ -3,10 +3,15 @@
     // if user is not logged in, they cannot access this page
     if (!isLoggedIn()) {
         $_SESSION['msg'] = "You must log in first";
-        header('location: ../login/index.php');
+        header('location: /project_1/app/login/index.php');
     }
 ?>
     <section>
+        <?php if (isAdmin()) {
+        print   "
+                <a href='../../admin/create_event.php'>Lisää tapahtuma</a>
+                ";
+    }?>
         <?php printEvents() ?>
     </section>
 <?php
