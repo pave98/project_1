2afd70ec274593b6a075a5e185c4ac4bc0b8af34
Niko Hienonen <niko.hienonen@gmail.com>
2018-12-12 17:02:17 +0200

    Joukkueen finishaus

--- app/joukkue/index.php
+++ app/joukkue/index.php
@@ -4,7 +4,7 @@
     <section class="s1">
         <div class="bodyContainer">
         <h1 class="mediumH1 nonCentered">RKC-Volley</h1>
-            <p class="intro">RKC-Volley on 2018 keväällä perustettu lentopallojoukkue Lempäälästä. Joukkueen perustivat 
+            <p class="intro">RKC-Volley on keväällä 2018 perustettu lentopallojoukkue Lempäälästä. Joukkueen perustivat 
             Joukkueenjohtaja Antti Koskinen ja kapteeni Niko Hienonen koska eivät löytäneet seuraa joka sopi
             juuri heidän tarpeisiinsa. Mukaan on tullut matkan varrella lisää sotureita ja pari lähtenyt, mutta
             joukkueen henki ja rähinä on silti kovempi kuin koskaan. Ota yhteyttä jos kiinnostuit toiminnasta!</p>
