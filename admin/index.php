b46973abf28dc029373c8df8a33d7f3d7bd102a0
Paavo Porkka <paavo.porkka@cs.tamk.fi>
2018-12-12 13:17:49 +0200

    user deletion style

--- admin/index.php
+++ admin/index.php
@@ -11,9 +11,9 @@
     <section class="s1">
         <h1 class="motto">Yhdessä Runkulla Admin</h1>
 
-        <h4><a href="create_user.php">Lisää käyttäjä</a></h1>
-        <h4><a href="create_event.php">Lisää tapahtuma</a></h1>
-        <h4><a href="delete_user.php">Poista käyttäjä</a></h4>
+        <a class="removeLink" href="create_user.php"><button class="inButton">Lisää käyttäjä</button></a>
+        <a class="removeLink" href="create_event.php"><button class="inButton">Lisää tapahtuma</button></a>
+        <a class="removeLink" href="delete_user.php"><button class="inButton">Poista käyttäjä</button></a>
     </section>
     
 <?php
