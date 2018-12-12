d29ca1fb293cb6f889cdeb9d089f5fd1897fabf4
Niko Hienonen <niko.hienonen@gmail.com>
2018-12-12 09:20:34 +0200

    adf

--- app/yhteystiedot/index.php
+++ app/yhteystiedot/index.php
@@ -2,7 +2,7 @@
     require "../header.php";
 ?>
     <section class="s1">
-        <h1>Yhteystiedot</h1>
+        <h1 class="mediumH1">Yhteystiedot</h1>
         
         <p class="contact"><i class="fas fa-mobile-alt"></i> +358 45 6313 113</p><br>
         <p class="contact"><i class="fas fa-envelope"></i> antti.a.koskinen@gmail.com</p>
